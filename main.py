7a2991290716b83b883a4af1753e2941403293e2
Noel <noemunrod@gmail.com>
2025-01-30 19:27:09 +0100

    Asignación de pines a los motores

---
+++ main.py
@@ -0,0 +1,28 @@
+"""Main script to run LineFollower conected to the Raspberry 4."""
+import RPi.GPIO as GPIO
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# TODO Insert here the GPIO Constants connected to the motors
+
+# TODO Insert here the GPIO Constants connected to the sensors
+
+# TODO Insert here a function to run a motor with a specific speed
+
+# TODO Insert here a function to stop a motor
+
+# TODO Insert here a function to read the sensors
+
+# TODO Insert here a function to change the motor speed based on the sensors readings
+
+# TODO Insert here a function to turn the robot based on the sensors readings
+
+# TODO Insert here a function to read the sensors and return the line position
+
+# TODO Insert here a function to run the motors based on the sensors readings
+
+# TODO Insert here the main loop to read the sensors and run the motors
+
+# Clean up the GPIO settings
+GPIO.cleanup()
